81c8094b1ea0d042effcd277deb11ac24ed2e175
Philip J. Fry <net.fedorax64@gmail.com>
2020-07-18 23:36:00 +0300

    Formatting in MessageHelper files has been fixed

--- NuGetSwitcher.Helper/IMessageHelper.cs
+++ NuGetSwitcher.Helper/IMessageHelper.cs
@@ -14,7 +14,7 @@ namespace NuGetSwitcher.Helper
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        void AddMessage(string message, TaskErrorCategory category, [CallerMemberName]string caller = "");
+        void AddMessage(string message, TaskErrorCategory category, [CallerMemberName] string caller = "");
 
         /// <summary>
         /// Displays a message 
@@ -27,14 +27,14 @@ namespace NuGetSwitcher.Helper
         /// <param name="project">
         /// Full path to the solution project.
         /// </param>
-        void AddMessage(string project, string message, TaskErrorCategory category, [CallerMemberName]string caller = "");
+        void AddMessage(string project, string message, TaskErrorCategory category, [CallerMemberName] string caller = "");
 
         /// <summary>
         /// Displays a message 
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        void AddMessage(Exception exception, [CallerMemberName]string caller = "");
+        void AddMessage(Exception exception, [CallerMemberName] string caller = "");
 
         /// <summary>
         /// Displays a message 
@@ -47,14 +47,14 @@ namespace NuGetSwitcher.Helper
         /// <param name="project">
         /// Full path to the solution project.
         /// </param>
-        void AddMessage(string project, Exception exception, [CallerMemberName]string caller = "");
+        void AddMessage(string project, Exception exception, [CallerMemberName] string caller = "");
 
         /// <summary>
         /// Displays a message 
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        void AddMessage(SwitcherException exception, [CallerMemberName]string caller = "");
+        void AddMessage(SwitcherException exception, [CallerMemberName] string caller = "");
 
         /// <summary>
         /// Clears previously created tasks 
--- NuGetSwitcher.Helper/MessageHelper.cs
+++ NuGetSwitcher.Helper/MessageHelper.cs
@@ -34,7 +34,7 @@ namespace NuGetSwitcher.Helper
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        public virtual void AddMessage(string message, TaskErrorCategory category, [CallerMemberName]string caller = "")
+        public virtual void AddMessage(string message, TaskErrorCategory category, [CallerMemberName] string caller = "")
         {
             ErrorList.Tasks.Add(new ErrorTask { Text = $"Caller: { caller }. Message: { message }", ErrorCategory = category });
         }
@@ -50,7 +50,7 @@ namespace NuGetSwitcher.Helper
         /// <param name="project">
         /// Full path to the solution project.
         /// </param>
-        public virtual void AddMessage(string project, string message, TaskErrorCategory category, [CallerMemberName]string caller = "")
+        public virtual void AddMessage(string project, string message, TaskErrorCategory category, [CallerMemberName] string caller = "")
         {
             ErrorList.Tasks.Add(new ErrorTask { Text = $"Caller: { caller }. Message: { message }", ErrorCategory = category, HierarchyItem = GetProjectHierarchyItem(project), Document = project });
         }
@@ -60,7 +60,7 @@ namespace NuGetSwitcher.Helper
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        public virtual void AddMessage(Exception exception, [CallerMemberName]string caller = "")
+        public virtual void AddMessage(Exception exception, [CallerMemberName] string caller = "")
         {
             SwitcherException se = exception as SwitcherException;
 
@@ -85,7 +85,7 @@ namespace NuGetSwitcher.Helper
         /// <param name="project">
         /// Full path to the solution project.
         /// </param>
-        public virtual void AddMessage(string project, Exception exception, [CallerMemberName]string caller = "")
+        public virtual void AddMessage(string project, Exception exception, [CallerMemberName] string caller = "")
         {
             AddMessage(project, exception.Message, TaskErrorCategory.Error, caller);
         }
@@ -95,7 +95,7 @@ namespace NuGetSwitcher.Helper
         /// with the specified 
         /// type in the associated GUI tab.
         /// </summary>
-        public virtual void AddMessage(SwitcherException exception, [CallerMemberName]string caller = "")
+        public virtual void AddMessage(SwitcherException exception, [CallerMemberName] string caller = "")
         {
             AddMessage(exception.MsbProject.FullPath, exception.Message, TaskErrorCategory.Error, caller);
         }
