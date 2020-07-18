98b0203b6fe7dd3edfa9e2780541fb64bbe0f2e6
Philip J. Fry <net.fedorax64@gmail.com>
2020-07-18 23:51:19 +0300

    AssmeblyInfo.cs has been updated

--- NuGetSwitcher/Properties/AssemblyInfo.cs
+++ NuGetSwitcher/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSIXProject1")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSIXProject1")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle  ("NuGetSwitcher")]
+[assembly: AssemblyProduct("NuGetSwitcher")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -29,5 +22,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion    ("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
